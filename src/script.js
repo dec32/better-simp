47a323c459ff9aaca3507e00de1468089893da6e
dec32 <1064103102@qq.com>
2024-05-30 04:00:55 +0800

    添加標籤過滤器

---
+++ src/script.js
@@ -0,0 +1,65 @@
+function toggle(filter) {
+    let on = [];
+    let off = [];
+    for (let filter of document.querySelector(".filters").querySelectorAll(".filter")) {
+        if (filter.classList.contains("disabled")) {
+            off.push(filter)
+        } else {
+            on.push(filter)
+        }
+    }
+    
+    let reset = false;
+    if (off.length == 0) {
+        // 在全选状态点击标签时，单选此标签
+        for (let f of document.querySelector(".filters").querySelectorAll(".filter")) {
+            if (f == filter) {
+                continue
+            }
+            f.classList.add("disabled")
+        }
+    } else if (off.length == 1){
+        // 选中最后一个标签，自然回到全选状态
+        filter.classList.remove("disabled");
+        reset = true
+    }else if (on.length == 1 && on[0] == filter) {
+        // 取消最后一个标签，强行跳回全选状态
+        for (let filter of document.querySelector(".filters").querySelectorAll(".filter")) {
+            filter.classList.remove("disabled")
+        }
+        reset = true
+    }  else if(filter.classList.contains("disabled")) {
+        // 普通的 flip 逻辑
+        filter.classList.remove("disabled")
+    } else {
+        // 普通的 flip 逻辑
+        filter.classList.add("disabled")
+    }
+    
+
+    if (reset) {
+        // 注意：全选时要把无标签的行也显示出来
+        for (let row of document.querySelectorAll(".row")) {
+            row.style.display = "flex"
+        }
+    } else {
+        let tags = []
+        for (let f of document.querySelector(".filters").querySelectorAll(".filter")) {
+            if (f.classList.contains("disabled")) {
+                continue
+            }
+            tags.push(f.innerHTML.substring(0, f.innerHTML.indexOf("(")))
+        }
+
+        for (let row of document.querySelectorAll(".row")) {
+            row.style.display = "none"
+            for (let tag of row.querySelectorAll(".tag")) {
+                tag = tag.innerHTML
+                if (tags.includes(tag)) {
+                    row.style.display = "flex"
+                    break
+                }
+            }
+        }
+    }
+}
