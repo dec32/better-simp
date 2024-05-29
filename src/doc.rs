3293c73d49cf7fd7a5ae9cd8ab744ffe18e892dc
dec32 <1064103102@qq.com>
2024-05-30 03:58:30 +0800

    添加標籤過滤器

--- src/doc.rs
+++ src/doc.rs
@@ -1,8 +1,9 @@
-use std::fs;
+use std::{collections::HashMap, fs, ops::AddAssign};
 
 use calamine::{open_workbook, Data, Range, Reader, Xlsx};
-use maud::{html, Markup, DOCTYPE};
+use maud::{html, Markup, PreEscaped, DOCTYPE};
 use crate::{parse_review, Review};
+
 
 impl Review {
     fn is_relevant(&self) -> bool {
@@ -29,7 +30,7 @@ fn row(review: Review) -> Markup {
         }
         div.comment {
             @for tag in review.tags {
-                span.tag { (tag) } " "
+                span.tag { (tag) }
             }
             (review.comment)
         }
@@ -37,34 +38,72 @@ fn row(review: Review) -> Markup {
 }
 
 // 生成单个表格
-fn table(title: &str, range: Range<Data>) -> Markup {
+fn table(title: &str, reviews: Vec<Review>) -> Markup {
     html!(
         div.group-title {(title)}
-        @for review in range.rows().skip(1).map(parse_review).filter(Review::is_relevant) {
+        @for review in reviews {
             div.row { (row(review)) }
         }
     )
 }
 
+fn collect_reviews(range: Range<Data>) -> Vec<Review> {
+    range.rows().skip(1).map(parse_review).filter(Review::is_relevant).collect()
+}
 
+
+fn sort_tags(tags: &mut [String], counts: &HashMap<String, u16>) {
+    tags.sort_by(|t1, t2| counts[t1].cmp(&counts[t2]).reverse())
+}
 
 // 生成文档
 pub fn gen(workbook_path: &str, output_path: &str) {
     let mut workbook: Xlsx<_> = open_workbook(workbook_path).expect("打开表格失败。");
-    let style = include_str!("style.css");
-    let tab_1 = table("表一", workbook.worksheet_range("表一").unwrap());
-    let tab_2 = table("表二", workbook.worksheet_range("表二").unwrap());
-    let other = table("其他", workbook.worksheet_range("其他").unwrap());
+
+    let mut tab_1 = collect_reviews(workbook.worksheet_range("表一").unwrap());
+    let mut tab_2 = collect_reviews(workbook.worksheet_range("表二").unwrap());
+    let mut other = collect_reviews(workbook.worksheet_range("其他").unwrap());
+
+    // 把标签按频度排序
+    let mut counts = HashMap::new();
+    for review in tab_1.iter().chain(tab_2.iter()).chain(other.iter()) {
+        for tag in review.tags.iter() {
+            // clone goes brrrrrrrr
+            counts.entry(tag.clone()).or_insert(0).add_assign(1);       
+        }
+    }
+
+    let mut tags = counts.keys().cloned().collect::<Vec<_>>();
+    sort_tags(&mut tags, &counts);
+    for review in tab_1.iter_mut().chain(tab_2.iter_mut()).chain(other.iter_mut()) {
+        sort_tags(&mut review.tags, &counts)
+    }
+
+    // 生成页面
+    let tab_1 = table("表一", tab_1);
+    let tab_2 = table("表二", tab_2);
+    let other = table("其他", other);
 
     let markup = html!(
         (DOCTYPE)
         header {
             title {"简化字批评"}
-            style { ( style )}
+            // link rel="stylesheet" href="https://unpkg.com/heti/umd/heti.min.css";
+            // script src="https://unpkg.com/heti/umd/heti-addon.min.js";
+            // script {
+            //     "const heti = new Heti('.heti');heti.autoSpacing();"
+            // }
+            script { ( PreEscaped(include_str!("script.js")) ) }
+            style { ( PreEscaped(include_str!("style.css")) ) }
         }
         body {
             div.main {
                 h1 {"简化字批评"}
+                div.filters {
+                    @for tag in tags {
+                        span.filter.tag onclick="toggle(this)" { ( tag )"("( counts[&tag] )")" }
+                    }
+                }
                 (tab_1)
                 (tab_2)
                 (other)
